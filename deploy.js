368cff15dbfceb91e201860b3c3eb2d6de6199c0
komal769 <JANGRAKOMAL06@GMAIL.COM>
2024-07-09 23:08:57 +0530

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const initBalance = 1;
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy(initBalance);
+  await assessment.deployed();
+
+  console.log(`A contract with balance of ${initBalance} ETH deployed to ${assessment.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
